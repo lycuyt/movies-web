f80c91d6a8323c3b2c039c6ac59a070641c9b861
becuyt <109753738+lycuyt@users.noreply.github.com>
2024-07-01 21:30:42 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import {Route, Routes} from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/traller/Trailer';
 function App() {
   const[movies, setMovies] = useState([]);
 
@@ -28,9 +30,11 @@ function App() {
   },[]);
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path = "/" element ={<Layout/>}>
           <Route path="/" element={<Home movies={movies} />} ></Route>
+          <Route path ="/Trailer/:ytTrailerId" element ={<Trailer/>}></Route>
         </Route>
       </Routes>
     </div>
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,8 +3,11 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import {Link, useNavigate} from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+// import {Link, useNavigate} from "react-router-dom";
+// import Button from 'react-bootstrap/Button';
 
 const Hero = ({ movies }) => {
     return (
@@ -23,19 +26,17 @@ const Hero = ({ movies }) => {
                                             <div className="movie-title">
                                                 <h4>{movie.title}</h4>
                                             </div>
-                                            <div className="movie-buttons-container">
+                                            <div className='movie-buttons-container'>
                                                 <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                                    <div className="play-button-icon-container">
-                                                        <FontAwesomeIcon className="play-button-icon"
-                                                            icon={faCirclePlay}
-                                                        />
+                                                    <div className='play-button-icon-container'>
+                                                        <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay} />
                                                     </div>
                                                 </Link>
-
                                                 {/* <div className="movie-review-button-container">
                                                     <Button variant="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
                                                 </div> */}
                                             </div>
+
                                         </div>
                                     </div>
                                 </div>
